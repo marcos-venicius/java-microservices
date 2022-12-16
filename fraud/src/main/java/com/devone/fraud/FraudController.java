d79ed92effac741f8ef2605a263e58cfb32655ee
Marcos Venicius <marcosdevelopment.business@gmail.com>
2022-12-15 22:13:42 -0300

    chore(fraud): log when check if customer is fraudulent

--- fraud/src/main/java/com/devone/fraud/FraudController.java
+++ fraud/src/main/java/com/devone/fraud/FraudController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/fraud-check")
 @AllArgsConstructor
@@ -15,6 +17,8 @@ public class FraudController {
 
   @GetMapping(path = "{customerId}")
   public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
+    log.info("check if customer {} is fraudulent", customerId);
+
     boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
 
     return new FraudCheckResponse(isFraudulentCustomer);
